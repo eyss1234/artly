f81aca03e80aab1be53e850150a7c03d459b502f
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-02 21:00:01 +0100

    rails routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :art_pieces, only: [:index, :show]
+
+  resources :users do
+    resources :art_pieces, only: %i[new create edit destroy]
+    resources :bookings, only: %i[new create edit destroy]
+  end
+
 end
