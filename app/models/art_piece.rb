7fcda65914f04de4dec940451502e43f79cdca38
Emily Santos <eyssantos@gmail.com>
2022-08-02 19:39:58 +0100

    add art_piece, booking, user associations

--- app/models/art_piece.rb
+++ app/models/art_piece.rb
@@ -1,3 +1,9 @@
 class ArtPiece < ApplicationRecord
   belongs_to :user
+
+  GENRES = ["abstract", "architecture", "landscape", "seascape", "portrait", "still life", "animals"]
+
+  validates_presence_of :name, :genre
+
+  validates :genre, inclusion: { in: GENRES }
 end
