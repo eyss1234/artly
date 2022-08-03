d8c05298cf37a1aa6e6d0693b2d400ffe53f4801
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2022-08-03 20:22:19 +0100

    db schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Seeding..."
+
+address = ["10 chesham road, london KT1 3AQ", "52 Sinclair Road, london W14 0NH", "63 Ashurst road, london EN4 9LH"]
+genre = ["abstract", "architecture", "landscape", "seascape", "portrait", "still life", "animals"]
+
+10.times do
+  user = User.create(
+    { email: Faker::Internet.email,
+      address: address.sample,
+      password: "123456"
+    }
+    )
+  user.save
+end
+
+10.times do
+  art = ArtPiece.create(
+   {
+     name: Faker::GreekPhilosophers.name,
+     user_id: rand(1..10),
+     description: Faker::GreekPhilosophers.quote,
+     genre: genre.sample,
+     cost_per_day: Faker::Commerce.price
+   }
+)
+art.save
+end
+
+10.times do
+ booking = Booking.create(
+  {
+    start_date: Faker::Date.in_date_period,
+    end_date:Faker::Date.in_date_period
+  }
+)
+booking.save
+end
+
+puts "Seeding done."
