0b63882bd677187ce9f0ee9da2489c13ce45408f
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-04 22:09:51 +0100

    initial steps for map - incomplete

--- app/models/art_piece.rb
+++ app/models/art_piece.rb
@@ -2,7 +2,7 @@ class ArtPiece < ApplicationRecord
   belongs_to :user
   has_many :bookings, :dependent => :restrict_with_error
   has_many_attached :photos
-  
+
   GENRES = ["abstract", "architecture", "landscape", "seascape", "portrait", "still life", "animals"]
 
   validates_presence_of :name, :genre, :description
