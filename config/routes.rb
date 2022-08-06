a626eea43c2f5851f9529a7c83cc79ae232fa504
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-06 17:45:32 +0100

    configured sign_out route to use destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
   root to: "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,11 +14,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :art_pieces, only: [:index, :show]
-
-  resources :users do
-    resources :art_pieces, only: %i[new create edit update destroy]
-    resources :bookings, only: %i[new create edit update destroy]
+  resources :art_pieces, only: [:new, :create, :edit, :update, :destroy, :index, :show] do
+    resources :bookings, only: %i[new create]
   end
+
+  resources :bookings, only: %i[edit update destroy]
+
 
 end
