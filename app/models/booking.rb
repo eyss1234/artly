20a832f616cd87e4f25d83c36aa96ee354ff53d5
Emily Santos <eyssantos@gmail.com>
2022-08-06 12:04:14 +0100

    testing booking validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :art_piece
-  # validates_presence_of :start_date
+  # validates_presence_of :start_date 
   
-  validates :end_date, presence: true, date: { after_or_equal_to: :start_date }
+  validates :end_date, presence: true
   validates :start_date, presence: true
 end
