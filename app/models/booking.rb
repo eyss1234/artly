e3bac92dfc540f1936cc1de4d6805564e35843bb
Emily Santos <eyssantos@gmail.com>
2022-08-06 12:01:43 +0100

    test change booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :art_piece
   # validates_presence_of :start_date
   
-  # validates :end_date, presence: true, date: { after_or_equal_to: :start_date }
+  validates :end_date, presence: true, date: { after_or_equal_to: :start_date }
+  validates :start_date, presence: true
 end
