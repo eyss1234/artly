bd5a4afd0552f090490705f6008a75eadb6dcd7c
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-08 21:56:47 +0100

    added delete option and bookings list on show page

--- app/models/art_piece.rb
+++ app/models/art_piece.rb
@@ -1,10 +1,16 @@
 class ArtPiece < ApplicationRecord
   belongs_to :user
-  has_many :bookings, :dependent => :restrict_with_error
+  has_many :bookings, dependent: :destroy
   has_many_attached :photos
 
   GENRES = ["abstract", "architecture", "landscape", "seascape", "portrait", "still life", "animals"]
 
   validates_presence_of :name, :genre, :description
   validates :genre, inclusion: { in: GENRES }
+
+  def live_bookings
+    return bookings.filter do |booking|
+      booking.end_date >= Date.today
+    end
+  end
 end
