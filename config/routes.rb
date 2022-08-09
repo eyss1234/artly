6c17d418bff8c3e2d5d6b96afeb0afa660785df4
Emily Santos <eyssantos@gmail.com>
2022-08-09 12:01:01 +0100

    add user show to routes, create user pundit, controller and basic view

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     resources :bookings, only: %i[new create]
   end
 
+  resources :users, only: %i[show]
   resources :bookings, only: %i[edit update destroy]
 
 end
