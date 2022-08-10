a1201977516c3fc7ba80efcc2896d49f39aa5a4c
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-10 13:55:33 +0100

    updated logic to deal with empty dates

--- app/controllers/art_pieces_controller.rb
+++ app/controllers/art_pieces_controller.rb
@@ -87,16 +87,24 @@ class ArtPiecesController < ApplicationController
   end
 
   def filter_by(params)
-    print(params)
+
     if params[:genres]
       @art_pieces = @art_pieces.filter do |art_piece|
         params[:genres].include?(art_piece.genre)
       end
     end
 
-    if params[:start_date] || params[:end_date]
-      @art_pieces = @art_pieces.filter do |art_piece|
-        art_piece.valid_booking?(params[:start_date], params[:end_date] || params[:start_date])
+    unless params[:start_date].nil? || (params[:start_date].empty? && params[:end_date].empty?)
+      params[:start_date] = params[:end_date] if params[:start_date].empty?
+      params[:end_date] = params[:start_date] if params[:end_date].empty?
+
+      if params[:start_date] || params[:end_date]
+        @art_pieces = @art_pieces.filter do |art_piece|
+          art_piece.valid_booking?(
+            params[:start_date] || params[:end_date],
+            params[:end_date] || params[:start_date]
+          )
+        end
       end
     end
   end
