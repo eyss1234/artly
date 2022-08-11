8b5a98e12cead3562c45c6b7f753e50a3dd51b0c
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-11 18:06:40 +0100

    first validations and associations done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  belong_to :user, through: :booking
+  belongs_to :art_piece, through: :booking
+
+  validates :title, presence: true
+  validates :details, presence: true
 end
