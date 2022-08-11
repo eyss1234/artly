6b24141dda4104d364477b6d11fac9fafb0a7dbb
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-11 22:21:50 +0100

    stripped out record, model, etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_204223) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_211236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,16 +65,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_204223) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.string "title"
-    t.text "review"
-    t.integer "rating"
-    t.bigint "booking_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["booking_id"], name: "index_reviews_on_booking_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -94,5 +84,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_204223) do
   add_foreign_key "art_pieces", "users"
   add_foreign_key "bookings", "art_pieces"
   add_foreign_key "bookings", "users"
-  add_foreign_key "reviews", "bookings"
 end
