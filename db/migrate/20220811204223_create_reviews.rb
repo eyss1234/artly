f9d879bddd74e4e02ff27f2b9d970bd4c69bb9d2
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2022-08-11 21:44:50 +0100

    review model

--- db/migrate/20220811204223_create_reviews.rb
+++ db/migrate/20220811204223_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.string :title
-      t.text :details
+      t.text :review
       t.integer :rating
       t.references :booking, null: false, foreign_key: true
 
