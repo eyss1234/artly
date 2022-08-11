030f0531aa091771a4afd32ed46f5ad29959e1d5
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-11 21:04:58 +0100

    reversed the model for review to start from scratch with Misha

--- app/models/review.rb
+++
@@ -1,8 +0,0 @@
-class Review < ApplicationRecord
-  belongs_to :booking
-  belong_to :user, through: :booking
-  belongs_to :art_piece, through: :booking
-
-  validates :title, presence: true
-  validates :details, presence: true
-end
