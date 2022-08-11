7fe38d944c3a984d9a6887a4cb74d393f9424ae6
Emily Santos <eyssantos@gmail.com>
2022-08-11 23:32:31 +0100

    add stimulus controller for total cost

--- app/javascript/controllers/total_price_controller.js
+++ app/javascript/controllers/total_price_controller.js
@@ -6,15 +6,18 @@ export default class extends Controller {
     console.log('Hello from total_price controller')
   }
 
-  static targets = ["secondDate", "firstDate", "price"]
+  static targets = ["secondDate", "firstDate"]
 
   update(event) {
-    const date1 = new Date(this.firstDateTarget.value);
-    const date2 = new Date(this.secondDateTarget.value);
-    if (date2 === NaN) {
+    const date1 = this.firstDateTarget.value;
+    const date2 = this.secondDateTarget.value;
+    const this.firstDateTarget.dataset.cost;
+    if (date2 === "") {
       return 1
     } else {
-      const diffTime = Math.abs(date2 - date1);
+      fstDate = Date.new(date1)
+      sndDate = Date.new(date2)
+      const diffTime = Math.abs(fstDate - sndDate);
       const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
       console.log(diffDays)
       return diffDays
