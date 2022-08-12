038b81a207d27f12f9635ebe66ef75caae6d8e8b
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2022-08-12 18:24:04 +0100

    add valications to rating model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :art_piece
 
-  has_many :rating
+  has_many :ratings
 
   validates :start_date, presence: true
   validates :end_date, presence: true
