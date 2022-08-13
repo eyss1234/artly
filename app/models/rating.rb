9359e7cf03a858566c721141e51b1d263e14d42d
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-13 13:46:05 +0100

    removing double validation

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,13 +3,7 @@ class Rating < ApplicationRecord
   belongs_to :art_piece, :through => :booking
   belongs_to :user, :through => :booking
 
-
-  validates :title, presence: true
-  validates :review, presence: true
-  validates :rating, presence: true
-
   validates :title, presence: true, length: { minimum: 3 }
   validates :review, presence: true, length: { minimum: 10 }
-  validates :rating, { in: 1..5 }
-
+  validates :rating, presence: true, { in: 1..5 }
 end
