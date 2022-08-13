b80f93bbf0a09f0e95547832db2c1c71cabb2102
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-13 21:43:31 +0100

    ratings listed in descending order on show page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :art_piece
 
-  has_many :ratings
+  has_one :rating
 
   validates :start_date, presence: true
   validates :end_date, presence: true
