f06d8263a4f1fc22b3c4d63f703ae4ad48ffef55
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-13 14:19:40 +0100

    updated model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,9 +1,7 @@
 class Rating < ApplicationRecord
   belongs_to :booking
-  belongs_to :art_piece, through: :booking
-  belongs_to :user, through: :booking
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :review, presence: true, length: { minimum: 10 }
-  validates :rating, presence: true # numericality: { in: 1..5 }
+  validates :rating, presence: true, numericality: { in: 1..5 }
 end
